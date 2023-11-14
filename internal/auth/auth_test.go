a3eae42cf12428c46bc73b1e18c7f525d81046e7
Tayyab <tayyabilyas963@gmail.com>
2023-11-14 07:09:20 +0530

    go fmt brr

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -5,34 +5,32 @@ import (
 	"testing"
 )
 
-
-
-func TestGetApiKey(t *testing.T){
-	t.Run("ValidAuthorizationHeader", func(t *testing.T){
+func TestGetApiKey(t *testing.T) {
+	t.Run("ValidAuthorizationHeader", func(t *testing.T) {
 		headers := http.Header{}
 		headers.Set("Authorization", "ApiKey 12345")
-        apiKey, err:=GetAPIKey(headers)
-		if err != nil{
+		apiKey, err := GetAPIKey(headers)
+		if err != nil {
 			t.Errorf("Expected no error, but got %v", err)
 		}
-		if apiKey!="12345"{
+		if apiKey != "12345" {
 			t.Errorf("Expected apiKey to be '12345', but got %s", apiKey)
 		}
 	})
 	//Test case with no auth header
-	t.Run("NoAuthorizationHeader", func (t *testing.T)  {
+	t.Run("NoAuthorizationHeader", func(t *testing.T) {
 		headers := http.Header{}
 		_, err := GetAPIKey(headers)
-		if err==nil {
+		if err == nil {
 			t.Error("Expected an error, but got none")
 		}
-		
+
 	})
 	// Test case with malformed auth header
-	t.Run("MalformedAuthorizationHeader", func (t *testing.T)  {
+	t.Run("MalformedAuthorizationHeader", func(t *testing.T) {
 		headers := http.Header{}
 		headers.Set("Authorization", "InvlaidFormat")
-		_, err:=GetAPIKey(headers)
+		_, err := GetAPIKey(headers)
 		if err == nil {
 			t.Error("Expected an error, but got none")
 		}
